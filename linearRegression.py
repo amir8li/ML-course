f966fc4436832ca613f34ba8bd0b5db57d0a709a
Amirali <amiralirahnamania@yahoo.com>
2026-02-13 16:46:24 +0330

    Training and Testing

--- linearRegression.py
+++ linearRegression.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import yfinance as yf
 import math
+import numpy as np
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
 
 def get_quandl_style_data(ticker, start_date='2020-01-01', end_date='2023-12-31'):
     t = yf.Ticker(ticker)
@@ -37,8 +40,21 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 for_cast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
+print(forecast_out)
 
 df['label'] = df[for_cast_col].shift(-forecast_out)
 df.dropna(inplace=True)
 
-print(df.head())
+X = np.array(df.drop(['label'], axis=1))
+y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(f'Accuracy: {accuracy}')
