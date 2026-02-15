f0e55fdf80229e13298ecc747fd2e9a8d730a032
Amirali <amiralirahnamania@yahoo.com>
2026-02-15 20:34:35 +0330

    adding predicting via both graph and sklearn

--- linearRegression.py
+++ linearRegression.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import yfinance as yf
-import math
+import math, datetime
 import numpy as np
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 def get_quandl_style_data(ticker, start_date='2020-01-01', end_date='2023-12-31'):
     t = yf.Ticker(ticker)
@@ -40,15 +44,15 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 for_cast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
-print(forecast_out)
 
 df['label'] = df[for_cast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'], axis=1))
-y = np.array(df['label'])
-
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+df.dropna(inplace=True)
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
@@ -57,4 +61,28 @@ clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 
-print(f'Accuracy: {accuracy}')
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+
+df.index = df.index.tz_localize(None)
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+print(df.head())
+print("-----------------------------")
+print(df.tail())
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')  
+plt.ylabel('Price')
+plt.show()
