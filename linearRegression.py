57ecb5d9b87731a853369c5aa287e8751c9f3f86
Amirali <amiralirahnamania@yahoo.com>
2026-02-27 13:24:34 +0330

    done with Linear Regression

--- linearRegression.py
+++ linearRegression.py
@@ -45,7 +45,7 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 for_cast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
-forecast_out = int(math.ceil(0.01 * len(df)))
+forecast_out = int(math.ceil(0.1 * len(df)))
 
 df['label'] = df[for_cast_col].shift(-forecast_out)
 
@@ -59,13 +59,13 @@ y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
-# clf = LinearRegression(n_jobs=-1)
-# clf.fit(X_train, y_train)
-# with open('linearregression.pickle', 'wb') as f:
-#     pickle.dump(clf, f)
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+with open('linearregression.pickle', 'wb') as f:
+    pickle.dump(clf, f)
 
-pickle_in = open('linearregression.pickle', 'rb')
-clf = pickle.load(pickle_in)
+# pickle_in = open('linearregression.pickle', 'rb')
+# clf = pickle.load(pickle_in)
 accuracy = clf.score(X_test, y_test)
 
 forecast_set = clf.predict(X_lately)
